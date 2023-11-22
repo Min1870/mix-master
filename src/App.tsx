10fc69a8e2bcc4f0c11d5b971ee86adea7b0605f
minkhantzaw <minkhantzaw@g.com>
2023-11-22 15:47:19 +0630

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  NewsLetter,
+  Cocktail,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "cocktail",
+        element: <Cocktail />,
+      },
+      {
+        path: "newsletter",
+        element: <NewsLetter />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/pages/SinglePageError.tsx
@@ -0,0 +1,9 @@
+
+
+const SinglePageError = () => {
+  return (
+    <div>SinglePageError</div>
+  )
+}
+
+export default SinglePageError
